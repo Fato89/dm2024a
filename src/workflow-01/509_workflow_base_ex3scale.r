cc34676fc2f7862c7b013ef5b2afe9703c5950e9
Fato89 <fdjp1@hotmail.com>
2024-05-31 14:09:21 -0300

    experimento_escala

--- src/workflow-01/509_workflow_base_ex3scale.r
+++ src/workflow-01/509_workflow_base_ex3scale.r
@@ -364,7 +364,7 @@ ZZ_final_base9 <- function( pinputexps )
   param_local$train$meseta <- 2001
 
   param_local$kaggle$envios_desde <-  9000L
-  param_local$kaggle$envios_hasta <- 12500L
+  param_local$kaggle$envios_hasta <- 13500L
   param_local$kaggle$envios_salto <-   500L
   param_local$kaggle$competition <- "itba-data-mining-2024-a"
 
